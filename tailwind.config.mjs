9bb77c14a3f582477bcf9bf3e575d3808d9d4f92
Sergio Luis Arrieta <slarrieta02@gmail.com>
2024-03-22 15:16:03 -0500

    nueva confi de nav

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,20 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				primary: {
+					DEFAULT: '#111827',
+					hover: '#343e4a'
+				},
+				secundary: '#343e4a',
+				accent: {
+					DEFAULT: '#cda274',
+					secundary: '#cda274',
+					hover: '#343e4a'
+				},
+			}
+		},
 	},
 	plugins: [],
 }
